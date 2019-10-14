4edbf376c8fc1595b0e127f05cc10c77da70943e
snakeice45 <jonas171192@gmail.com>
2019-10-14 13:05:21 -0500

    update from the loop proyect with java second commit

--- src/com/exercise6loop/app/ForLoop.java
+++ src/com/exercise6loop/app/ForLoop.java
@@ -3,8 +3,44 @@ package com.exercise6loop.app;
 public class ForLoop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//constants declaration
+		
+		//variable declaration
+		
+		//objects construction
+		
+		//create a series from 1 to 100
+		for(int i=0; i<=100; i++)
+		{
+			System.out.println(i);
+		}
+		for(int i=100; i>=1; i--)
+		{
+			System.out.println(i);
+		}		
+		for(int i=0; i<=100; i+=2)
+		{
+			System.out.println(i);
+		}
+		
+		System.out.println("Display odd numbers from 100 to 200");
+		for(int i=100; i<200;i++)
+		{
+			if(i%2 == 0)
+			{
+				continue; //breaks the cycle and start from the beginning
+			}
+			System.out.println(i);
+		}
+		System.out.println("Display just the half of the numbers in the cycle");
+		for(int i=100; i<200;i++)
+		{
+			if(i == 150)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
 	}
 
 }
